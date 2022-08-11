95c2cf55bda3004d3a1a2450596298a7e6418c59
Mi Do <mi.doyen93@gmail.com>
2022-08-11 14:56:37 -0400

    first

--- LudoGame.py
+++ LudoGame.py
@@ -278,14 +278,4 @@ class LudoGame:
             if self._board[current_board] == 0:
                 self._board[current_board] = {player.get_position(): token}
 
-players = ['A', 'B']
-turns = [('A', 6), ('A', 4), ('A', 5), ('A', 4), ('B', 6), ('B', 4), ('B', 1), ('B', 2), ('A', 6), ('A', 4), ('A', 6), ('A', 3), ('A', 5), ('A', 1), ('A', 5), ('A', 4)]
-game = LudoGame()
-current_tokens_space = game.play_game(players, turns)
-player_A = game.get_player_by_position('A')
-print(player_A.get_completed())
-print(player_A.get_token_p_step_count())
-print(current_tokens_space)
-player_B = game.get_player_by_position('B')
-print(player_B.get_space_name(55))
-print(game.get_board())
+
