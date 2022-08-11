798447fee5bf9089e8beb2bb3d8f929461fa7463
Mi Do <mi.doyen93@gmail.com>
2022-08-11 11:50:52 -0400

    first

--- LudoGame.py
+++ LudoGame.py
@@ -21,11 +21,11 @@ class Player:
         self._position = None  # A, B, C, or D
         self._start = None
         self._end = None
-        self._currentP = -1
-        self._currentQ = -1
+        self._currentP = 'H'
+        self._currentQ = 'H'
         self._state = False  # F: still playing, T: won and finished the game
-        self._stepP = 0
-        self._stepQ = 0
+        self._stepP = -1
+        self._stepQ = -1
 
     def set_position(self, pos):
         """
@@ -43,7 +43,7 @@ class Player:
         if pos == 'B':
             self._start = 15
             self._end = 8
-        if pos == 'c':
+        if pos == 'C':
             self._start = 29
             self._end = 22
         if pos == 'D':
@@ -72,13 +72,13 @@ class Player:
         """
         set the current position of token p
         """
-        self._currentP = num
+        self._currentP = self.get_space_name(num)
 
     def set_current_q(self, num):
         """
         set the current position of token q
         """
-        self._currentQ = num
+        self._currentQ = self.get_space_name(num)
 
     def set_state(self):
         """
@@ -111,6 +111,18 @@ class Player:
         Otherwise, return False
         """
         return self._state
+
+    def get_token_p_current(self):
+        """
+        returns the current position of token P
+        """
+        return self._currentP
+
+    def get_token_q_current(self):
+        """
+        returns the current position of token P
+        """
+        return self._currentQ
 
     def get_token_p_step_count(self):
         """
@@ -149,8 +161,8 @@ class LudoGame:
         """
         initializes the private data members
         """
-        self._list_players = {}  #'A': p, q, stacked
-        self._board = []
+        self._list_players = {}
+        self._board = [0] * 57
 
     def play_game(self, players_area, turns1):
         """
@@ -166,13 +178,53 @@ class LudoGame:
             player.set_start_end(pos)
             self._list_players[pos] = player
 
-        # move token according to the turns list
-        for turn in turns:
-            flag = False
-            stalk = False
-            player = turn[0]
-            player = self._list_players[player]
-            num = turn
+        # move token according to the turn list
+        for turn in turns1:
+            # who's turn with roll
+            who = turn[0]
+            player = self._list_players[who]
+            roll = turn[1]
+            print(who)
+
+            step_p = player.get_token_p_step_count()
+            step_q = player.get_token_q_step_count()
+
+            # if roll = 6, let token out if still in home yard
+            if roll == 6:
+                if step_p == -1:
+                    step_p += 1
+                    player.set_step_p(1)
+                    player.set_current_p(step_p)
+                    print('start p')
+                    pass
+                elif step_q == -1:
+                    step_q += 1
+                    player.set_step_q(1)
+                    player.set_current_q(step_q)
+                    print('start q')
+                    pass
+
+            # otherwise
+            if step_p + roll == 57:
+                self.move_token(player, 'p', roll)
+            elif step_q + roll == 57:
+                self.move_token(player, 'q', roll)
+            elif self._board[step_p + roll] is not None and step_p + roll != step_q:
+                self.move_token(player, 'p', roll)
+            elif self._board[step_q + roll] is not None and step_q + roll != step_p:
+                self.move_token(player, 'q', roll)
+            else:
+                if step_p == -1:
+                    self.move_token(player, 'q', roll)
+                elif step_q == -1:
+                    self.move_token(player, 'p', roll)
+                elif step_p < step_q:
+                    self.move_token(player, 'p', roll)
+                elif step_p > step_q:
+                    self.move_token(player, 'q', roll)
+                else:
+                    self.move_token(player, 'p', roll)
+                    self.move_token(player, 'q', roll)
 
     def get_player_by_position(self, pos):
         """
@@ -194,9 +246,6 @@ class LudoGame:
         follows the priority rule
         """
         pass
-
-
-
 
 
 players = ['A', 'B']
